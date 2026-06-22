241f9f0f6e30389572197ac1bc7fece51bd152c9
Mihail-Korotkov <mixail.korotkov.01@bk.ru>
2026-06-22 20:19:58 +0300

    Registered simple tag for categories

--- app/urls.py
+++ app/urls.py
@@ -19,8 +19,9 @@ from argparse import Namespace
 from django.contrib import admin
 from django.urls import include, path
 from debug_toolbar.toolbar import debug_toolbar_urls
+from django.conf.urls.static import static
 
-from app.settings import DEBUG
+from django.conf import settings
 
 
 urlpatterns = [
@@ -28,5 +29,9 @@ urlpatterns = [
     path('',include('main.urls',namespace='main')),
     path('catalog/',include('goods.urls',namespace='catalog')),
 ] 
-if DEBUG:
+if settings.DEBUG:
     urlpatterns += debug_toolbar_urls()
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+
+
--- main/views.py
+++ main/views.py
@@ -4,11 +4,10 @@ from goods.models import Categories
 
 # Create your views here.
 def index(request):
-    categories = Categories.objects.all
     context = {
         'title':'HOMIK',
         'content':'Магазин мебели HOME',
-        'categories' : categories,
+        
         
         }
 
