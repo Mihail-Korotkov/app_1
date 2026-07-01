b46b4dcc984487c07cbd262f57c9188cef49194e
Mihail-Korotkov <mixail.korotkov.01@bk.ru>
2026-07-01 19:24:30 +0300

    postgresql database conected & fullt textsearch

--- goods/utils.py
+++ goods/utils.py
@@ -1,5 +1,5 @@
 from django.db.models import Q
-
+from django.contrib.postgres.search import SearchVector
 from goods.models import Products
 
 
@@ -7,13 +7,17 @@ def q_search(query):
     if query.isdigit() and len(query) <= 5:                             # поиск по id
         return Products.objects.filter(id = int(query))
     
-    keywords = [word for word in query.split() if len(word) > 2]
-    q_objects = Q()
+    return Products.objects.annotate(search=SearchVector("name", "description")).filter(search=query)
 
-    for token in keywords:
-        q_objects |= Q(description__contains = token)
-        q_objects |= Q(description__contains = token.capitalize())
-        q_objects |= Q(name__contains = token)
-        q_objects |= Q(name__contains = token.capitalize())
-    return Products.objects.filter(q_objects)
+
+    
+    # keywords = [word for word in query.split() if len(word) > 2]
+    # q_objects = Q()
+
+    # for token in keywords:
+    #     q_objects |= Q(description__contains = token)
+    #     q_objects |= Q(description__contains = token.capitalize())
+    #     q_objects |= Q(name__contains = token)
+    #     q_objects |= Q(name__contains = token.capitalize())
+    # return Products.objects.filter(q_objects)
 
